c25e4cc9905569649d47417627cb6b0a4f5a6361
prefie <prefie@bk.ru>
2023-01-07 18:28:25 +0500

    module5-task1

--- project/src/components/film/filmCard.tsx
+++ project/src/components/film/filmCard.tsx
@@ -1,20 +1,53 @@
-import { Dispatch, FC, SetStateAction } from 'react';
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { VideoPlayer } from '../video-player/VideoPlayer';
+import { Film } from '../../pages/main/main.models';
 
 type Props = {
-  id: number;
-  title: string;
-  posterImagePath: string;
+  film: Film;
   onHover?: Dispatch<SetStateAction<number>>;
 }
 
-export const FilmCard: FC<Props> = (props) => (
-  <article className="small-film-card catalog__films-card" onMouseEnter={() => props.onHover?.((_) => props.id)} onMouseLeave={() => props.onHover?.((_) => -1)}>
-    <div className="small-film-card__image">
-      <img src={props.posterImagePath} alt={props.title} width="280" height="175"/>
-    </div>
-    <h3 className="small-film-card__title">
-      <Link className="small-film-card__link" to={`/films/${props.id}`}>{props.title}</Link>
-    </h3>
-  </article>
-);
+export const FilmCard: FC<Props> = (props) => {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+
+  useEffect(() => {
+    let startPlaying = true;
+    const forceSetIsPlaying = () => {
+      if (startPlaying) {
+        setIsPlaying(true);
+      }
+    }
+
+    if (isHovering) {
+      setTimeout(forceSetIsPlaying, 1000);
+    }
+
+    return (() => {
+      startPlaying = false;
+    });
+  }, [isHovering]);
+
+  const handleMouseEnter = () => {
+    props.onHover?.((_) => props.film.id);
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    props.onHover?.((_) => -1);
+    setIsHovering(false);
+    setIsPlaying(false);
+  };
+
+  return (
+    <article className="small-film-card catalog__films-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="small-film-card__image">
+        <VideoPlayer muted film={props.film} isPlaying={isPlaying} width={280} height={175}/>
+      </div>
+      <h3 className="small-film-card__title">
+        <Link className="small-film-card__link" to={`/films/${props.film.id}`}>{props.film.title}</Link>
+      </h3>
+    </article>
+  );
+};
